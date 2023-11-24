fe6352324a615506ddc81281aae31fdb0c262df2
Youss0203 <youssefhalibi003@gmail.com>
2023-11-24 10:03:37 +0100

    inizio correzzione

--- script/script.js
+++ script/script.js
@@ -1,14 +1,18 @@
 const giorniCountDown = document.getElementById("giorni");
 const oreCountDown = document.getElementById("ore");
 const minutiCountDown = document.getElementById("minuti");
+const secondiCountDown = document.getElementById("secondi")
 
-const countDown=setInterval(function(){
-    let nuovoAnno = new Date("1 january 2024")
-    let anno = new Date()
-    let risultato = nuovoAnno - anno
+let nuovoAnno = new Date("2024, 0 ,1,00,00,00")
+let anno = new Date()
+let risultato = nuovoAnno.getTime() - anno.getTime();
 
-    const days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
 
-},1000)
+const days = Math.floor(risultato / (1000 * 60 * 60 * 24));
+const hours = Math.floor((risultato % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((risultato % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((risultato % (1000 * 60)) / 1000);
+
+
+
+
